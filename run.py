332c6fcf0a327caa70d59b1b2c87610504a1875f
AlexanderJWard <alexjamesward@hotmail.com>
2022-09-01 23:17:37 +0000

    Add player guess function and tweak validation function

--- run.py
+++ run.py
@@ -10,24 +10,30 @@ class Game:
         self.player_name = player_name
         self.tries = 5
         self.guess_correct = False
-        self.guess_input = input("Enter a letter or word: ").upper()
 
-    def guess_validation(self):
+    def data_validation(self):
+        self.validation = False
+        while not self.validation:
             try:
+                self.guess_input = input("Enter a letter or word: ").upper()
                 if not self.guess_input.isalpha():
                     raise TypeError(
-                    f"Your guess {self.guess_input} does not contain alphabet letters.")
+                        f"Your guess {self.guess_input} does not contain alphabet letters.")
+                else:
+                    self.validation = True
             except TypeError as e:
                 print(f"{e} Please enter a new guess")
 
     def player_guess(self):
-        self.guess_input
-        Game.guess_validation(self)
+        Game.data_validation(self)
+        print("Data Type Correct...")
+        print(self.guess_input)
 
 
 def main():
-    print("Welcome")
+    print("Hangman\n")
     player_name = input("Please enter your name: ")
+    print(f"\nWelcome {player_name}!")
     Game(player_name).player_guess()
 
 
