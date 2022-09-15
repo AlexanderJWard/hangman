17610be3ec8a295a1133a2750bb70aa3ac4101a8
AlexanderJWard <alexjamesward@hotmail.com>
2022-09-15 10:32:08 +0000

    Import colorama library

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from google.oauth2.service_account import Credentials
 import random
 from words import hangman_words
 from natsort import natsorted
+from colorama import init
+from colorama import Fore, Back, Style
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -15,51 +17,6 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman hiscores')
 WORKSHEET = SHEET.worksheet('hiscores')
-
-
-class colors:
-    """
-    Colors class:reset all colors with colors.reset; two
-    sub classes fg for foreground
-    and bg for background; use as colors.subclass.colorname.
-    i.e. colors.fg.red or colors.bg.greenalso, the generic bold, disable,
-    underline, reverse, strike through,
-    and invisible work with the main class i.e. colors.bold
-    """
-    reset = '\033[0m'
-    bold = '\033[01m'
-    disable = '\033[02m'
-    underline = '\033[04m'
-    reverse = '\033[07m'
-    strikethrough = '\033[09m'
-    invisible = '\033[08m'
- 
-    class fg:
-        black = '\033[30m'
-        red = '\033[31m'
-        green = '\033[32m'
-        orange = '\033[33m'
-        blue = '\033[34m'
-        purple = '\033[35m'
-        cyan = '\033[36m'
-        lightgrey = '\033[37m'
-        darkgrey = '\033[90m'
-        lightred = '\033[91m'
-        lightgreen = '\033[92m'
-        yellow = '\033[93m'
-        lightblue = '\033[94m'
-        pink = '\033[95m'
-        lightcyan = '\033[96m'
- 
-    class bg:
-        black = '\033[40m'
-        red = '\033[41m'
-        green = '\033[42m'
-        orange = '\033[43m'
-        blue = '\033[44m'
-        purple = '\033[45m'
-        cyan = '\033[46m'
-        lightgrey = '\033[47m'
 
 
 class Hangman:
@@ -122,8 +79,8 @@ class Hangman:
             self.wins = int(WORKSHEET.col_values(2)[hiscore_index])
             self.loses = int(WORKSHEET.col_values(3)[hiscore_index])
             print(f"\nWelcome back {self.player_name}!")
-            print(f"\nGames Won: {self.wins}")
-            print(f"Games Lost: {self.loses}\n")
+            print(Fore.GREEN + f"\nGames Won: {self.wins}")
+            print(Fore.RED + f"Games Lost: {self.loses}\n")
         else:
             WORKSHEET.append_row(leaderboard)
             print(f"\nWelcome {self.player_name}. Good Luck!")
